9f23a0b59a23dbcf8d33e8993b7eb2cc161d3c0d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-13 16:36:44 +0530

    Created a database server project

---
+++ database-server/src/main/java/com/cg/databaseserver/DatabaseServerApplication.java
@@ -0,0 +1,13 @@
+package com.cg.databaseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseServerApplication.class, args);
+	}
+
+}
