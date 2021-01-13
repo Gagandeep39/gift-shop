7f8705f497b8d657c1a1b45352c5a0d1e3f20ac7
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-13 16:49:23 +0530

    Added a custom logger

---
+++ database-server/src/main/java/com/cg/databaseserver/config/CustomLogger.java
@@ -0,0 +1,39 @@
+package com.cg.databaseserver.config;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Slf4j
+@Component
+public class CustomLogger {
+
+  @Pointcut("execution(* com.cg.*.*.*.*(..)))")
+  public void everyWhere() {
+  }
+
+  @Before("everyWhere()")
+  public void logBefore(JoinPoint point) {
+    log.debug("@Before class: " + point.getTarget().getClass().getSimpleName());
+    log.debug("@Before method: " + point.getSignature().toShortString());
+    log.debug("Arguments: ");
+    Object[] objects = point.getArgs();
+    for (Object object : objects) {
+      log.debug("->" + object);
+    }
+  }
+
+  @AfterReturning(pointcut = "everyWhere()", returning = "result")
+  public void logAfter(JoinPoint point, Object result) {
+    log.debug("@AfterReturning class: " + point.getTarget().getClass().getSimpleName());
+    log.debug("@AfterReturning method: " + point.getSignature().toShortString());
+    log.debug("Returning Value: " + result);
+  }
+
+}
