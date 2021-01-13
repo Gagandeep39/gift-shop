5c6d82b00ff22ede075dabda64a9cf8375966044
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-13 19:17:22 +0530

    Added more dummy data

--- database-server/src/main/resources/data-h2.sql
+++ database-server/src/main/resources/data-h2.sql
@@ -35,6 +35,8 @@ INSERT INTO product_category(category_id, category_name, category_description, c
 
 INSERT INTO product_category(category_id, category_name, category_description, category_image_url) VALUES (100002, 'Toys', 'an object for a child to play with', 'https://taiken.co/uploads/2019/05/Kawaii_Culture_001-1200x1047.jpg');
 
+-- TODO Add 4 more category
+
 ALTER sequence category_id_sequence restart with 100007;
 
 INSERT INTO cart VALUES (100001);
@@ -43,3 +45,11 @@ INSERT INTO cart VALUES (100003);
 INSERT INTO cart VALUES (100004);
 INSERT INTO cart VALUES (100005);
 INSERT INTO cart VALUES (100006);
+
+INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category) VALUES (100001, 'Teddy Bear', 1200, 99, 'A stuffed toy is a toy doll with an outer fabric sewn from a textile and stuffed with flexible material', 'https://www.wellandgood.com/wp-content/uploads/2019/01/Stocksy-Teddy-Bear-Marta-Locklear.jpg', 'ENABLED', 100002);
+
+INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category) VALUES (100002, 'Color Pencils', 200, 99, 'Colored drawing pencil for kids', 'https://previews.123rf.com/images/kenishirotie/kenishirotie1801/kenishirotie180100009/92727823-spectrum-color-pencils-set-arranged-in-s-curve-isolated-on-white-background.jpg', 'ENABLED', 100001);
+
+-- TODO Add 9 more items
+
+ALTER sequence category_id_sequence restart with 1000011;
