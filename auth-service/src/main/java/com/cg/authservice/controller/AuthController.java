6de28622295678a7642fa9a52e0b909847747e4b
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-14 18:35:04 +0530

    Created auth controller

--- auth-service/src/main/java/com/cg/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/cg/authservice/controller/AuthController.java
@@ -7,8 +7,11 @@
  */
 package com.cg.authservice.controller;
 
+import java.util.Map;
+
 import javax.validation.Valid;
 
+import com.cg.authservice.dto.ForgotPasswordRequest;
 import com.cg.authservice.dto.LoginRequest;
 import com.cg.authservice.dto.LoginResponse;
 import com.cg.authservice.services.AuthService;
@@ -16,7 +19,10 @@ import com.cg.authservice.services.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +40,17 @@ public class AuthController {
   @PostMapping("/login")
   public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
     return ResponseEntity.status(HttpStatus.OK).body(authService.login(loginRequest));
+  }
+  
+  @GetMapping("/forgotpassword/{username}")
+  public ResponseEntity<Map<String, String>> forgotPassword(@PathVariable String username) {
+    return ResponseEntity.status(HttpStatus.OK).body(authService.fetchSecurityQuestionForUser(username));
+  }
+
+  @PutMapping("/forgotpassword")
+  public ResponseEntity<Map<String, String>> validateAnswerAndUpdatePassword(
+      @Valid @RequestBody ForgotPasswordRequest forgotPasswordRequest) {
+    return ResponseEntity.status(HttpStatus.OK).body(authService.validateAnswerAndUpdate(forgotPasswordRequest));
   }
 
 }
