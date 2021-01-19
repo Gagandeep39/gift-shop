f3f8428fb42c778191c3cff69ed6f23f35034ea1
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-20 00:51:19 +0530

    Created cart repository

---
+++ auth-service/src/main/java/com/cg/authservice/repositories/CartRepository.java
@@ -0,0 +1,11 @@
+package com.cg.authservice.repositories;
+
+import com.cg.authservice.entities.Cart;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+  
+}
