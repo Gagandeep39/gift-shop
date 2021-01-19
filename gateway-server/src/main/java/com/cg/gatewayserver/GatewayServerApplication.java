1a1ea79b5ed44920b38095c924ce935dc165b0c6
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-19 22:51:16 +0530

    Added zuul annotation

--- gateway-server/src/main/java/com/cg/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/cg/gatewayserver/GatewayServerApplication.java
@@ -1,9 +1,18 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2021-01-19 22:50:26
+ * @modify date 2021-01-19 22:50:26
+ * @desc [description]
+ */
 package com.cg.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
