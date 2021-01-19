2df9263cbeea4703e54e26bf831f46d3e999245f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-19 22:45:32 +0530

    Added authorship

--- discovery-server/src/main/java/com/cg/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/cg/discoveryserver/DiscoveryServerApplication.java
@@ -1,3 +1,10 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2021-01-19 22:45:23
+ * @modify date 2021-01-19 22:45:23
+ * @desc [description]
+ */
 package com.cg.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
