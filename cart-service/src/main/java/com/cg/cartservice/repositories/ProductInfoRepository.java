6dd6280c4b88169bbc8c976c041fd89135487dd9
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-20 13:31:49 +0530

    Fixed jpa query logic

--- cart-service/src/main/java/com/cg/cartservice/repositories/ProductInfoRepository.java
+++ cart-service/src/main/java/com/cg/cartservice/repositories/ProductInfoRepository.java
@@ -3,11 +3,13 @@ package com.cg.cartservice.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 import com.cg.cartservice.entities.ProductInfo;
 
 @Repository
 public interface ProductInfoRepository extends JpaRepository<ProductInfo, Long> {
 	
-	public ProductInfo findByproductId(long productId);
+	Optional<ProductInfo> findByproductId(long productId);
 
 }
