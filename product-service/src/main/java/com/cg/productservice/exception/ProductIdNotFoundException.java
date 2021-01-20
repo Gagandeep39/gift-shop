e139ae12807b65ad89fd40a227ee998b35809573
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-20 10:05:06 +0530

    Added exceptions

---
+++ product-service/src/main/java/com/cg/productservice/exception/ProductIdNotFoundException.java
@@ -0,0 +1,15 @@
+package com.cg.productservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductIdNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	private String errorName;
+	private String errorDescription;
+}
