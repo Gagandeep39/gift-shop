e723685bf13d9467321318b1a455e6cefbc689bd
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-20 10:05:22 +0530

    Added validations

--- product-service/src/main/java/com/cg/productservice/entities/ProductCategory.java
+++ product-service/src/main/java/com/cg/productservice/entities/ProductCategory.java
@@ -12,6 +12,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,8 +31,15 @@ public class ProductCategory  {
     @SequenceGenerator(name = "category_id_sequence", initialValue = 100000, allocationSize = 1)
     @GeneratedValue(generator = "category_id_sequence", strategy = GenerationType.SEQUENCE)
     private Long categoryId;
+
+    @NotNull
+    @NotBlank
     private String categoryName;
+    @NotNull
+    @NotBlank
     private String categoryDescription;
+    @NotNull
+    @NotBlank
     private String categoryImageUrl;
 
 }
