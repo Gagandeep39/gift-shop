0ed30bf459fb09ee4c265c05c8b7e86e51c6975f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-20 22:40:11 +0530

    Enabled scheduling

--- order-service/src/main/java/com/cg/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/cg/orderservice/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.cg.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
