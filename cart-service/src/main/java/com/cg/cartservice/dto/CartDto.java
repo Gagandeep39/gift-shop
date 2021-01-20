27d2f3d8c4c472f0e86e8926cb582b3fe0370f7f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-20 12:03:09 +0530

    Created DTOs

--- cart-service/src/main/java/com/cg/cartservice/dto/CartDto.java
+++ cart-service/src/main/java/com/cg/cartservice/dto/CartDto.java
@@ -1,5 +1,46 @@
 package com.cg.cartservice.dto;
 
+import java.util.List;
+
+import com.cg.cartservice.entities.UserDetails;
+
 public class CartDto {
-  
+	
+	//private int userId;
+
+	 
+
+    private List<ItemDto> itemDtoList;
+
+
+
+public CartDto() {}    
+    
+	public CartDto( List<ItemDto> itemDtoList) {
+		super();
+
+		this.itemDtoList = itemDtoList;
+	}
+
+
+
+	
+
+
+
+
+
+
+	public List<ItemDto> getItemDtoList() {
+		return itemDtoList;
+	}
+
+
+
+	public void setItemDtoList(List<ItemDto> itemDtoList) {
+		this.itemDtoList = itemDtoList;
+	}
+
+ 
+
 }
--- cart-service/src/main/java/com/cg/cartservice/dto/ItemDto.java
+++ cart-service/src/main/java/com/cg/cartservice/dto/ItemDto.java
@@ -3,5 +3,25 @@ package com.cg.cartservice.dto;
 public class ItemDto {
 
   private Long productId;
+  private int quantity;
+  
+  
+public ItemDto(Long productId, int quantity) {
+	super();
+	this.productId = productId;
+	this.quantity = quantity;
+}
+public Long getProductId() {
+	return productId;
+}
+public void setProductId(Long productId) {
+	this.productId = productId;
+}
+public int getQuantity() {
+	return quantity;
+}
+public void setQuantity(int quantity) {
+	this.quantity = quantity;
+}
   
 }
