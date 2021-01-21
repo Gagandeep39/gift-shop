29e26ec7b595bfbdaae6eb816e2b058466da8050
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-22 01:31:36 +0530

    Created request password page

---
+++ frontend/src/app/components/auth/request-password/request-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-password',
+  templateUrl: './request-password.component.html',
+  styleUrls: ['./request-password.component.css']
+})
+export class RequestPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
