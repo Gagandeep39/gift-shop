5b68ac1be4a28d2e79e676f916250de445478f65
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-21 11:32:58 +0530

    Added environments

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  protocol: 'http://',
+  applicationUrl: 'localhost:9000',
+  authService: 'auth-service',
+  productService: 'product-service',
+  orderService: 'order-service',
+  paymentService: 'payment-service',
+  cartService: 'cart-service',
 };
 
 /*
