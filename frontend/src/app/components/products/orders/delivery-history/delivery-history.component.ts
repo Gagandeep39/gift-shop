5323b33b24b47c2fa62ee1772bd5839130c2f95b
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-21 12:16:03 +0530

    Created products related components

---
+++ frontend/src/app/components/products/orders/delivery-history/delivery-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-history',
+  templateUrl: './delivery-history.component.html',
+  styleUrls: ['./delivery-history.component.css']
+})
+export class DeliveryHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
