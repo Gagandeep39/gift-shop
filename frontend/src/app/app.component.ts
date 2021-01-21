0d03765a17a7241f68a5d9e1c8e0b6b062c2b6a1
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-21 19:21:46 +0530

    Removed dummt content

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
-import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'frontend';
 
-  constructor(private authService: AuthService) {}
-  logOut() {
-    this.authService.logout();
-  }
+  constructor() {}
 }
