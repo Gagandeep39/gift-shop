9b28c09241e272e149274dc1143d84c7141d4409
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-21 16:59:21 +0530

    Added login logic

--- frontend/src/app/components/auth/login/login.component.ts
+++ frontend/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  submitted = false;
+  loginSubscription: Subscription;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    public loadingService: LoadingService
+  ) {}
+
+  ngOnDestroy(): void {
+    if (this.loginSubscription) this.loginSubscription.unsubscribe();
+  }
 
   ngOnInit(): void {
+    this.authService.redirectIfLoggedIn();
+    this.initForm();
+  }
+
+  login() {
+    this.submitted = true;
+    if (this.loginForm.valid) this.submitData(this.loginForm.value);
+  }
+
+  submitData(formData: any) {
+    this.loadingService.enableLoading();
+    this.loginSubscription = this.authService.login(formData).subscribe(
+      (response) => {
+        this.router.navigate(['/']);
+        this.loadingService.disableLoading();
+      },
+      (error) => {
+        this.loadingService.disableLoading();
+        if (error.error.message === 'FieldException')
+          error.error.errors.forEach((element) =>
+            this.loginForm.controls[element.field]?.setErrors({
+              serverValidationError: element.message,
+            })
+          );
+        else throw new Error(error);
+      }
+    );
+  }
+
+  initForm() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
   }
 
 }
