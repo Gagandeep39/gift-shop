6dd52f224aa13f0ebbdbbad331ecde9741a90d9f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-22 12:11:16 +0530

    Added dynamic navigation items

--- frontend/src/app/shared/navigation/navigation.component.ts
+++ frontend/src/app/shared/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { EventService } from 'src/app/services/event.service';
 
 @Component({
   selector: 'app-navigation',
@@ -8,9 +9,47 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor(private authService: AuthService) {}
-  ngOnInit(): void {
+  isLoggedIn;
+  loginSubscription;
+  activeTabs;
+  userTabs = [
+    { name: 'About', link: 'about' },
+    { name: 'Cart', link: 'about' },
+    { name: 'My Purchase', link: 'about' },
+  ];
+  anonymousTab = [
+    { name: 'About', link: 'about' },
+    { name: 'Login', link: '/login' },
+    { name: 'Register', link: '/register' },
+  ];
+  adminTabs= [
+    { name: 'add', link: 'add' },
+    { name: 'view', link: 'view' },
+  ];;
+
+  constructor(
+    private authService: AuthService,
+    private eventService: EventService
+  ) {
+    this.initializeNavItems();
   }
+  initializeNavItems() {
+    this.eventService.loggedInUser.subscribe(res => {
+      if (res ===null) {
+        this.isLoggedIn = false;
+        this.activeTabs = this.anonymousTab;
+      }else if (this.isLoggedIn?.role === 'Admin') {
+        this.isLoggedIn = true;
+        this.activeTabs = this.adminTabs;
+      } else {
+        this.isLoggedIn = true;
+        this.activeTabs = this.userTabs;
+      }
+      
+    })
+  }
+
+  ngOnInit(): void {}
 
   logOut() {
     this.authService.logout();
