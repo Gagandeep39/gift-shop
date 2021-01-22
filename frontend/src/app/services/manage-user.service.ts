3307abb2c39d1e8ccdcd47c0aa4723e5a32f8106
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-22 11:38:14 +0530

    Created registration srevice

---
+++ frontend/src/app/services/manage-user.service.ts
@@ -0,0 +1,35 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2021-01-22 11:38:04
+ * @modify date 2021-01-22 11:38:04
+ * @desc [description]
+ */
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ManageUserService {
+  authServiceUrl = `${environment.protocol}${environment.applicationUrl}/${environment.authService}`;
+
+  constructor(private http: HttpClient) {}
+
+  fetchAllUsers() {
+    return this.http.get(`${this.authServiceUrl}/register`);
+  }
+
+  updateUser(formData) {
+    return this.http.put(`${this.authServiceUrl}/register`, formData);
+  }
+
+  addUser(formData) {
+    return this.http.post(`${this.authServiceUrl}/register`, formData);
+  }
+
+  fetchById(id) {
+    return this.http.get(`${this.authServiceUrl}/register/${id}`);
+  }
+}
