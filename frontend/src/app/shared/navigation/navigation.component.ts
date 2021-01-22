fc7d061d61371e7800135848c5fcaf4daed082df
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-22 12:33:23 +0530

    Fixed navigation logic

--- frontend/src/app/shared/navigation/navigation.component.ts
+++ frontend/src/app/shared/navigation/navigation.component.ts
@@ -8,45 +8,53 @@ import { EventService } from 'src/app/services/event.service';
   styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent implements OnInit {
-
-  isLoggedIn;
-  loginSubscription;
-  activeTabs;
+  isLoggedIn: boolean;
+  activeTabs = [];
   userTabs = [
-    { name: 'About', link: 'about' },
-    { name: 'Cart', link: 'about' },
-    { name: 'My Purchase', link: 'about' },
+    { name: 'About', link: '/about' },
+    { name: 'Cart', link: '/products/cart' },
+    { name: 'My Purchase', link: '/products/orders' },
   ];
   anonymousTab = [
-    { name: 'About', link: 'about' },
+    { name: 'About', link: '/about' },
     { name: 'Login', link: '/login' },
     { name: 'Register', link: '/register' },
   ];
-  adminTabs= [
-    { name: 'add', link: 'add' },
-    { name: 'view', link: 'view' },
-  ];;
+  adminTabs = [
+    { name: 'Add', link: '/admin/add' },
+    { name: 'View', link: '/admin/view' },
+  ];
 
   constructor(
     private authService: AuthService,
     private eventService: EventService
   ) {
+    this.initializeApp();
     this.initializeNavItems();
   }
+  initializeApp() {
+    // If user data is present and token not expired, emi event to update navi
+    if (this.authService.isAuthenticated()) {
+      this.eventService.loggedInUser.next(
+        this.authService.fetchFromSessionStorage()
+      );
+    }
+    // Log out user if toke already expired
+    else this.authService.logout();
+  }
   initializeNavItems() {
-    this.eventService.loggedInUser.subscribe(res => {
-      if (res ===null) {
+    this.eventService.loggedInUser.subscribe((res) => {
+      if (res === null) {
         this.isLoggedIn = false;
         this.activeTabs = this.anonymousTab;
-      }else if (this.isLoggedIn?.role === 'Admin') {
+      } else if (res?.role === 'Admin') {
         this.isLoggedIn = true;
         this.activeTabs = this.adminTabs;
       } else {
         this.isLoggedIn = true;
         this.activeTabs = this.userTabs;
       }
-      
-    })
+    });
   }
 
   ngOnInit(): void {}
