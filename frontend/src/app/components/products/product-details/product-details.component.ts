b99e8ca94e3a16d85ecdbf650b9f40e0c72ebfe8
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-25 03:12:13 +0530

    Improved details logic

--- frontend/src/app/components/products/product-details/product-details.component.ts
+++ frontend/src/app/components/products/product-details/product-details.component.ts
@@ -1,27 +1,49 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-
   count = 1;
   maxQuantity = 10;
+  productImageUrl = '/assets/images/celebration2.jpg';
+  productName;
+  productQuantity;
+  productDescription;
+  productCategory;
+  productPrice;
 
-  constructor() { }
+  constructor(
+    public loadingService: LoadingService,
+    private router: Router,
+    private location: Location
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   increment() {
     if (this.count >= this.maxQuantity) this.count = this.maxQuantity;
-    else this.count ++;
+    else this.count++;
   }
   decrement() {
     if (this.count <= 1) this.count = 1;
-    else this.count --;
+    else this.count--;
   }
 
+  addNewItem(productId) {
+    this.loadingService.enableLoading();
+    setTimeout(() => {
+      this.loadingService.disableLoading();
+      this.router.navigateByUrl('/');
+    }, 2000);
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
