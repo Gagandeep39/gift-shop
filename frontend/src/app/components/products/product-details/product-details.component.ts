0d221baf8aa3a94eb858def23f16bca03d4b01bd
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-25 02:57:37 +0530

    Added increment decrement quantity

--- frontend/src/app/components/products/product-details/product-details.component.ts
+++ frontend/src/app/components/products/product-details/product-details.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
+  count = 1;
+  maxQuantity = 10;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  increment() {
+    if (this.count >= this.maxQuantity) this.count = this.maxQuantity;
+    else this.count ++;
+  }
+  decrement() {
+    if (this.count <= 1) this.count = 1;
+    else this.count --;
   }
 
 }
