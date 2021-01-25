c571b1ccee29d7d3233a0ef39239fb2cb80ab44c
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-25 23:48:47 +0530

    Created placehlder dedlivery component

--- frontend/src/app/components/products/orders/delivery-history/delivery-history.component.ts
+++ frontend/src/app/components/products/orders/delivery-history/delivery-history.component.ts
@@ -1,15 +1,47 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2021-01-25 22:36:58
+ * @modify date 2021-01-25 22:36:58
+ * @desc [description]
+ */
 import { Component, OnInit } from '@angular/core';
+import { DeliveryHistory } from 'src/app/models/delivery-history.model';
 
 @Component({
   selector: 'app-delivery-history',
   templateUrl: './delivery-history.component.html',
-  styleUrls: ['./delivery-history.component.css']
+  styleUrls: ['./delivery-history.component.css'],
 })
 export class DeliveryHistoryComponent implements OnInit {
+  deliveryHistory: DeliveryHistory[] = [
+    {
+      orderId: 100001,
+      deliveryId: 100001,
+      orderStatus: 'NEW',
+      updatedOn: new Date('2021-01-21'),
+    },
+    {
+      orderId: 100001,
+      deliveryId: 100002,
+      orderStatus: 'DISPATCHED',
+      updatedOn: new Date('2021-01-22'),
+    },
+    {
+      orderId: 100001,
+      deliveryId: 100003,
+      orderStatus: 'OUT_FOR_DELIVERY',
+      updatedOn: new Date('2021-01-23'),
+    },
+    {
+      orderId: 100001,
+      deliveryId: 100004,
+      orderStatus: 'DELIVERED',
+      updatedOn: new Date('2021-01-24'),
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
