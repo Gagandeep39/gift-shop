ed00f57271bda39637082f54a770aef507bad71b
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-26 23:57:14 +0530

    Updated main sql database

--- database-server/src/main/resources/data-h2.sql
+++ database-server/src/main/resources/data-h2.sql
@@ -46,33 +46,33 @@ INSERT INTO cart VALUES (100004);
 INSERT INTO cart VALUES (100005);
 INSERT INTO cart VALUES (100006);
 
-INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category) VALUES (100001, 'Teddy Bear', 1200, 99, 'A stuffed toy is a toy doll with an outer fabric sewn from a textile and stuffed with flexible material', 'https://www.wellandgood.com/wp-content/uploads/2019/01/Stocksy-Teddy-Bear-Marta-Locklear.jpg', 'ENABLED', 100002);
+INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category, discount_percent) VALUES (100001, 'Teddy Bear', 1200, 99, 'A stuffed toy is a toy doll with an outer fabric sewn from a textile and stuffed with flexible material', 'https://www.wellandgood.com/wp-content/uploads/2019/01/Stocksy-Teddy-Bear-Marta-Locklear.jpg', 'ENABLED', 100002, 20);
 
-INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category) VALUES (100002, 'Color Pencils', 200, 99, 'Colored drawing pencil for kids', 'https://previews.123rf.com/images/kenishirotie/kenishirotie1801/kenishirotie180100009/92727823-spectrum-color-pencils-set-arranged-in-s-curve-isolated-on-white-background.jpg', 'ENABLED', 100001);
+INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category, discount_percent) VALUES (100002, 'Color Pencils', 200, 99, 'Colored drawing pencil for kids', 'https://previews.123rf.com/images/kenishirotie/kenishirotie1801/kenishirotie180100009/92727823-spectrum-color-pencils-set-arranged-in-s-curve-isolated-on-white-background.jpg', 'ENABLED', 100001, 20);
 
-INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category) VALUES (100003, 'Color Pencils', 300, 99, 'Coloring book for kids', 'https://images-na.ssl-images-amazon.com/images/I/61+7VR7+dsL._SX360_BO1,204,203,200_.jpg', 'ENABLED', 100001);
+INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category, discount_percent) VALUES (100003, 'Color Pencils', 300, 99, 'Coloring book for kids', 'https://images-na.ssl-images-amazon.com/images/I/61+7VR7+dsL._SX360_BO1,204,203,200_.jpg', 'ENABLED', 100001, 20);
 
-INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category) VALUES (100004, 'Coloring Book', 200, 99, 'Colored drawing', 'https://cdn.shopify.com/s/files/1/2486/2582/products/118-202-Color-In-Book-Enchanting-Unicorns-B1_88e62295-d705-41d7-ad68-8a9b8c4c7ba7_800x.png', 'ENABLED', 100001);
+INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category, discount_percent) VALUES (100004, 'Coloring Book', 200, 99, 'Colored drawing', 'https://cdn.shopify.com/s/files/1/2486/2582/products/118-202-Color-In-Book-Enchanting-Unicorns-B1_88e62295-d705-41d7-ad68-8a9b8c4c7ba7_800x.png', 'ENABLED', 100001, 30);
 
-INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category) VALUES (100005, 'Action Figure', 200, 299, 'Toys for kids', 'https://www.staractionfigures.co.uk/user/products/large/yfghngfjhtrju.jpg', 'ENABLED', 100002);
+INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category, discount_percent) VALUES (100005, 'Action Figure', 200, 299, 'Toys for kids', 'https://www.staractionfigures.co.uk/user/products/large/yfghngfjhtrju.jpg', 'ENABLED', 100002, 30);
 
-INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category) VALUES (100006, 'Action Figure', 200, 399, 'Toys for kids', 'https://www.meijer.com/content/dam/meijer/product/0063/05/0980/90/0063050980908_0_A1C1_1200.png', 'ENABLED', 100002);
+INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, product_category, discount_percent) VALUES (100006, 'Action Figure', 200, 399, 'Toys for kids', 'https://www.meijer.com/content/dam/meijer/product/0063/05/0980/90/0063050980908_0_A1C1_1200.png', 'ENABLED', 100002, 30);
 
 -- TODO Add 25 more items
 
 ALTER sequence product_id_sequence restart with 100026;
 
 -- ORDER Table
-INSERT INTO ORDER_MAIN (order_id, buyer_address, buyer_city, buyer_email, buyer_pincode, buyer_state, buyer_name, buyer_phone, order_amount, order_status, payment_type, payment_id, user_id) VALUES (100001, 'Powai, near D-Mart', 'Mumbai', 'gagandeep@mail.com', '400072', 'Maharashtra', 'Gagandeep Singh', '8419969059', 4000, 'DISPATCHED', 'ONLINE', '100001', 100001);
+INSERT INTO ORDER_MAIN (order_id, buyer_address, buyer_city, buyer_email, buyer_pincode, buyer_state, buyer_name, buyer_phone, order_amount, order_status, payment_type, payment_id, user_id, discounted_amount) VALUES (100001, 'Powai, near D-Mart', 'Mumbai', 'gagandeep@mail.com', '400072', 'Maharashtra', 'Gagandeep Singh', '8419969059', 4000, 'DISPATCHED', 'ONLINE', '100001', 100001, 840);
 -- TODO Add 4 more items
 
 ALTER sequence order_id_sequence restart with 100002;
 
 -- To be purchased product
 
-INSERT INTO product_in_order (product_in_order_id, cart_id, order_id, product_id, product_name, product_description, product_icon, product_category, product_price, product_stock) VALUES (100001, 100001, null, 100001, 'Teddy Bear', 'A stuffed toy is a toy doll with an outer fabric sewn from a textile and stuffed with flexible material', 'https://www.wellandgood.com/wp-content/uploads/2019/01/Stocksy-Teddy-Bear-Marta-Locklear.jpg', 'Toys', 1200, 1);
+INSERT INTO product_in_order (product_in_order_id, cart_id, order_id, product_id, product_name, product_description, product_icon, product_category, product_price, product_stock, discount_percentage) VALUES (100001, 100001, null, 100001, 'Teddy Bear', 'A stuffed toy is a toy doll with an outer fabric sewn from a textile and stuffed with flexible material', 'https://www.wellandgood.com/wp-content/uploads/2019/01/Stocksy-Teddy-Bear-Marta-Locklear.jpg', 'Toys', 1200, 1, 20);
 
-INSERT INTO product_in_order (product_in_order_id, cart_id, order_id, product_id, product_name, product_description, product_icon, product_category, product_price, product_stock) VALUES (100002, null, 100001, 100001, 'Large Teddy Bear', 'A stuffed toy is a toy doll with an outer fabric sewn from a textile and stuffed with flexible material', 'https://www.wellandgood.com/wp-content/uploads/2019/01/Stocksy-Teddy-Bear-Marta-Locklear.jpg', 'Toys', 1200, 1);
+INSERT INTO product_in_order (product_in_order_id, cart_id, order_id, product_id, product_name, product_description, product_icon, product_category, product_price, product_stock, discount_percentage) VALUES (100002, null, 100001, 100001, 'Large Teddy Bear', 'A stuffed toy is a toy doll with an outer fabric sewn from a textile and stuffed with flexible material', 'https://www.wellandgood.com/wp-content/uploads/2019/01/Stocksy-Teddy-Bear-Marta-Locklear.jpg', 'Toys', 1200, 1, 20);
 
 -- TODO Add 9 more items
 
