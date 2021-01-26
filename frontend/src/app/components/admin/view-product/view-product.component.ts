35ce2c155d36829a9435532bd6e5053d7ca098c4
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-26 16:44:19 +0530

    Added view logic in admin panel

--- frontend/src/app/components/admin/view-product/view-product.component.ts
+++ frontend/src/app/components/admin/view-product/view-product.component.ts
@@ -3,37 +3,29 @@ import { Router } from '@angular/router';
 import { Product } from 'src/app/models/product.model';
 import { ProductService } from 'src/app/services/product.service';
 
-
-
 @Component({
   selector: 'app-view-product',
   templateUrl: './view-product.component.html',
-  styleUrls: ['./view-product.component.css']
+  styleUrls: ['./view-product.component.css'],
 })
 export class ViewProductComponent implements OnInit {
+  products: Product[] = [];
 
-  products : Product[] = [];
+  constructor(private service: ProductService, private route: Router) {}
 
-
-  constructor(private service : ProductService, private route : Router) { }
-
-  ngOnInit() : void {
+  ngOnInit(): void {
+    this.fetchAllProducts();
+  }
+  fetchAllProducts() {
     this.service.fetchAllProducts().subscribe((data: Product[]) => {
       this.products = data;
       console.log(this.products);
     });
   }
 
-  deleteProduct(productID : number){
-      this.service.deleteProduct(productID).subscribe(response => {
-        // console.log(data);
-        this.route.navigate(['admin/view']);
-      });
+  deleteProduct(productID: number) {
+    this.service.deleteProduct(productID).subscribe((response) => this.fetchAllProducts());
   }
 
-  updateProduct(product :Product){
-    
-  }
-
-
+  updateProduct(product: Product) {}
 }
