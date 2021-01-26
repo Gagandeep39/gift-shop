51e57d40b0cb1e915811d4bdf5b2c1917d1dc28b
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-27 02:49:35 +0530

    Updated order bean

--- auth-service/src/main/java/com/cg/authservice/entities/OrderMain.java
+++ auth-service/src/main/java/com/cg/authservice/entities/OrderMain.java
@@ -54,6 +54,9 @@ public class OrderMain {
     private String buyerPincode;
     private BigDecimal orderAmount;
     private BigDecimal discountedAmount;
+    private BigDecimal deliveryCharge;
+    private BigDecimal finalPrice;
+
 
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
