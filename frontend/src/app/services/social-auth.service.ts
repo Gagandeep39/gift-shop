b873855c24ec98369d12c00be2d54a7747866b9f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-01-30 01:12:09 +0530

    Fixed social login not updating navigation

--- frontend/src/app/services/social-auth.service.ts
+++ frontend/src/app/services/social-auth.service.ts
@@ -7,27 +7,44 @@
  */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { User } from '../models/user.model';
+import { AuthService } from './auth.service';
+import { EventService } from './event.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SocialAuthService {
-
-  
   authServiceUrl = `${environment.protocol}${environment.applicationUrl}/${environment.authService}/social`;
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private eventServie: EventService,
+    private authService: AuthService
+  ) {}
 
   validateGoogleTokenAndLogin(data) {
-    return this.http.post(`${this.authServiceUrl}/google`, data);
+    return this.http
+      .post(`${this.authServiceUrl}/google`, data)
+      .pipe(tap((user: User) => this.triggerLogin(user)));
   }
 
   validateFacebookTokenAndLogin(data) {
-    return this.http.post(`${this.authServiceUrl}/facebook`, data);
+    return this.http
+      .post(`${this.authServiceUrl}/facebook`, data)
+      .pipe(tap((user: User) => this.triggerLogin(user)));
   }
 
   socialSignUp(data) {
-    return this.http.post(`${this.authServiceUrl}/signup`, data);
+    return this.http
+      .post(`${this.authServiceUrl}/signup`, data)
+      .pipe(tap((user: User) => this.triggerLogin(user)));
+  }
+
+  triggerLogin(user: User) {
+    this.authService.saveToSessionStorage(user);
+    this.eventServie.loggedInUser.next(user);
   }
 }
