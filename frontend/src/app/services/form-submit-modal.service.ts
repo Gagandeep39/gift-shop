d89d3281eaa95a792b477c21767be4f2f026ccea
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-05 22:31:15 +0530

    Added form submit modal logic

--- frontend/src/app/services/form-submit-modal.service.ts
+++ frontend/src/app/services/form-submit-modal.service.ts
@@ -1,9 +1,34 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2021-02-05 22:31:03
+ * @modify date 2021-02-05 22:31:03
+ * @desc [description]
+ */
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FormSubmitModalService {
+  url: string;
+  message: string;
+  private display: BehaviorSubject<'open' | 'close'> = new BehaviorSubject(
+    'close'
+  );
 
-  constructor() { }
+  watch(): Observable<'open' | 'close'> {
+    return this.display.asObservable();
+  }
+
+  open(message, url) {
+    this.message = message;
+    this.url = url;
+    this.display.next('open');
+  }
+
+  close() {
+    this.display.next('close');
+  }
 }
