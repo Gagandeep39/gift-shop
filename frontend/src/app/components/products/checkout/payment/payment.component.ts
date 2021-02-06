f514dd4d7b4a784b91790275c9108ee8bb9e0f9f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-06 19:13:03 +0530

    Added total calculation logic

--- frontend/src/app/components/products/checkout/payment/payment.component.ts
+++ frontend/src/app/components/products/checkout/payment/payment.component.ts
@@ -9,6 +9,11 @@ import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { map, take, tap } from 'rxjs/operators';
+import { Address } from 'src/app/models/address.model';
+import { Cart } from 'src/app/models/cart.model';
+import { ProductInOrder } from 'src/app/models/product-in-order.model';
+import { CartService } from 'src/app/services/cart.service';
+import { ManageUserService } from 'src/app/services/manage-user.service';
 
 @Component({
   selector: 'app-payment',
@@ -16,22 +21,39 @@ import { map, take, tap } from 'rxjs/operators';
   styleUrls: ['./payment.component.css'],
 })
 export class PaymentComponent implements OnInit {
-  address = {
-    city: 'Mumbai',
-    state: 'Maharashtra',
-    area: 'Powai',
-    pincode: '400072',
-  };
-  deliveryCharge = 20;
+  address: Address;
+  deliveryCharge: number;
+  totalAfterDiscount: number;
+  totalBeforeDiscount: number;
+  finalTotal: number;
+  user;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private cartService: CartService,
+    private manageUserService: ManageUserService
   ) {}
 
   ngOnInit(): void {
-    // this.initDeliveryData();
+    this.initDeliveryData();
+    this.fetchCartData();
+    this.fetchUserData();
+  }
+  fetchUserData() {
+    this.manageUserService
+      .fetchLoggedInUserForEdit()
+      .pipe(take(1))
+      .subscribe({ next: (res) => (this.user = res) });
+  }
+  fetchCartData() {
+    this.cartService
+      .fetchCartByUserId()
+      .pipe(take(1))
+      .subscribe({
+        next: (res: Cart) => this.calculateOrderSummary(res.products),
+      });
   }
 
   initDeliveryData() {
@@ -56,4 +78,44 @@ export class PaymentComponent implements OnInit {
   payWithRazorpay() {}
 
   payWithStripe() {}
+
+  calculateOrderSummary(productsInOrder: ProductInOrder[]) {
+    console.log(productsInOrder);
+
+    let discountPrice = 0;
+    let totalBeforeDiscount = 0;
+    let finalTotal;
+    for (const key in productsInOrder) {
+      console.log(productsInOrder[key]);
+      totalBeforeDiscount += productsInOrder[key].productPrice;
+      if (productsInOrder[key].discountPercent === 0)
+        discountPrice = productsInOrder[key].productPrice;
+      else discountPrice += this.calculatePrice(productsInOrder[key]);
+    }
+    this.totalBeforeDiscount = totalBeforeDiscount;
+
+    this.totalAfterDiscount = discountPrice;
+    finalTotal = this.totalAfterDiscount + this.deliveryCharge;
+
+    this.finalTotal = finalTotal;
+  }
+
+  calculatePrice(currentProduct: ProductInOrder) {
+    return (
+      currentProduct.productPrice -
+      currentProduct.productPrice * 0.01 * currentProduct.discountPercent
+    );
+  }
+
+  checkOutInServer(res) {
+    const data = {
+      ...this.address,
+      deliveryCharge: this.deliveryCharge,
+      paymentId: res.paymentId,
+    };
+    this.cartService.checkout(data).subscribe((res) => {
+      console.log(res);
+      this.router.navigateByUrl('/products/delivery/' + res['orderId']);
+    });
+  }
 }
