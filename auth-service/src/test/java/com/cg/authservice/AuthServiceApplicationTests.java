4fb30dc9b0d2ec7fa32b5b908c04e4fe8c0dd786
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-08 12:36:08 +0530

    Added test profile

--- auth-service/src/test/java/com/cg/authservice/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/cg/authservice/AuthServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.cg.authservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = { "test" })
 class AuthServiceApplicationTests {
 
 	@Test
