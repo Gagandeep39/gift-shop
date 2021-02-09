ba7d6fb6ccb42eb8fca159ee977f16c159cdb9e4
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-09 13:26:30 +0530

    Added cart test cases

--- cart-service/src/test/java/com/cg/cartservice/CartServiceApplicationTests.java
+++ cart-service/src/test/java/com/cg/cartservice/CartServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.cg.cartservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = { "test" })
 class CartServiceApplicationTests {
 
 	@Test
