23d8d16d76292f9eb896bbc0647e1583168688e5
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-11 11:14:22 +0530

    Fixed failing test cases

--- order-service/src/test/java/com/cg/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/cg/orderservice/OrderServiceApplicationTests.java
@@ -2,7 +2,9 @@ package com.cg.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles(profiles = { "test" })
 @SpringBootTest
 class OrderServiceApplicationTests {
 
--- payment-service/src/test/java/com/cg/paymentservice/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/cg/paymentservice/PaymentServiceApplicationTests.java
@@ -2,7 +2,9 @@ package com.cg.paymentservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles(profiles = { "test" })
 @SpringBootTest
 class PaymentServiceApplicationTests {
 
