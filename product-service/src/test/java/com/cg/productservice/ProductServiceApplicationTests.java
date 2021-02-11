088f278de25c143125a6d0437fdab394e43cbf1e
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-02-11 11:14:51 +0530

    Added product controller test cases

--- product-service/src/test/java/com/cg/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/cg/productservice/ProductServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.cg.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = { "test" })
 class ProductServiceApplicationTests {
 
 	@Test
